a1b4dddac80e344c7869f65f15b1fc9546a41433
tahamehtab <taha.mehtab13@gmail.com>
2024-08-27 04:28:00 +0500

    text chatbot

---
+++ app.py
@@ -0,0 +1,28 @@
+import google.generativeai as genai
+import os
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+os.environ["API_KEY"] = os.getenv("API_KEY")
+
+genai.configure(api_key=os.environ["API_KEY"])
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    user_message = request.form['message']
+    bot_response = model.generate_content([user_message])
+
+    # Format the response for better readability
+    formatted_response = bot_response.text.replace('\n', '<br>').replace('  ', '&nbsp;&nbsp;').replace('*', '')
+
+    return jsonify({'response': formatted_response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
